d7ad788787cfbd316931984405fba8d2c581b531
Ty Bandachit <tbandachit@gmail.com>
2020-03-12 18:00:35 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,42 @@
+require 'pry'
 class BankAccount
+    #ready
+    attr_accessor :balance, :status
+    attr_reader :name
+    @@all = []
+    def initialize(name, balance = 1000)
+        @name = name
+        @balance = balance
+        @status = "open"
+        @@all << self
+    end
+
+    def deposit(amount)
+        @balance += amount
+    end
+
+    def display_balance
+        "Your balance is $#{self.balance}."
+    end
+
+    def valid? 
+        if @balance > 0 && @status == "open"
+            true
+        else 
+            false
+        end
+    end
+    
+    def close_account
+        self.status = "closed"
+    end
+
+    def self.all
+        @@all
+    end
 
 end
+
+# cindy = BankAccount.new("cindy")
+# binding.pry
+# puts 's'
