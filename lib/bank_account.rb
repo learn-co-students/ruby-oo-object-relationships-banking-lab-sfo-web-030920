4e49388e3b97692e35768f1bb0bca9c3a2fdad76
lalogaribo <garibo.eduardo90@gmail.com>
2020-03-12 10:38:34 -0700

    completed bankAccount class

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,28 @@
 class BankAccount
 
-end
+  attr_accessor :balance, :status
+  attr_reader :name
+
+  def initialize(name, balance = 1000, status = 'open')
+    @name = name
+    @balance = balance
+    @status = status
+  end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    @status == 'open' && balance > 0 ? true : false
+  end
+  
+  def close_account
+    @status = 'closed'
+  end
+  
+end退
